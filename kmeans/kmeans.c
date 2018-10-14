2aa674fab177be60d44294eb08343a5b22feb1b2
tidjungs <waewprach.s@ku.th>
2018-10-14 13:13:53 +0700

    add randome k centroid with unique

--- kmeans/kmeans.c
+++ kmeans/kmeans.c
@@ -1,40 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
   char filename[50];
   sprintf(filename, "./%s", argv[1]);
-  FILE* stream = fopen(filename, "r");
+  // FILE* stream = fopen(filename, "r");
 
   int row = atoi(argv[2]);
   int col = atoi(argv[3]);
+  int k = atoi(argv[4]);
 
-  int **data = (int **)malloc(row * sizeof(int *));
-  for (int i=0; i<row; i++)
-    data[i] = (int *)malloc(col * sizeof(int));
+  // random k centroids with unique position
+  int *centroids  = (int *)malloc(k * sizeof(int *));
+  for (int i=0; i<k; i++) {
+    centroids[i] = -1;
+  }
+  srand(time(NULL));
+  int ct = 0;
+  while (ct < k) {
+    int r = rand() % row;
+    int dup = 0;
+    for (int i=0; i<=ct; i++) {
+      if (r == centroids[i]) {
+        dup = 1;
+        break;
+      }
+    }
+    if (dup == 0) {
+      centroids[ct] = r;
+      ct++;
+    }
+  }
+  // for (int i=0; i<k; i++) {
+  //   printf("%d\n", centroids[i]);
+  // }
 
-  char line[1024];
-  int i = 0;
-  while(fgets(line, 1024, stream))
-  {
-    char *pch;
-    pch = strtok(line,",");
-    int j = 0;
-    while(pch != NULL)
-    {
-      data[i][j] = atoi(pch);
-      pch = strtok(NULL, ",");
-      j++;
-    }
-    i++;
-  }
-  for (int i = 0; i<row; i++) {
-    for (int j = 0; j<col; j++) {
-      printf("%d ", data[i][j]);
-    }
-    printf("\n");
-  }
-  free(data);
+  // int **data = (int **)malloc(row * sizeof(int *));
+  // for (int i=0; i<row; i++)
+  //   data[i] = (int *)malloc(col * sizeof(int));
+
+  // char line[1024];
+  // int i = 0;
+  // while(fgets(line, 1024, stream))
+  // {
+  //   char *pch;
+  //   pch = strtok(line,",");
+  //   int j = 0;
+  //   while(pch != NULL)
+  //   {
+  //     data[i][j] = atoi(pch);
+  //     pch = strtok(NULL, ",");
+  //     j++;
+  //   }
+  //   i++;
+  // }
+  // for (int i = 0; i<row; i++) {
+  //   for (int j = 0; j<col; j++) {
+  //     printf("%d ", data[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+  // free(data);
   return 0;
 }
