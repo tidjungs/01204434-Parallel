5c8d331f7e1955167d6913989467d06eaae678cf
tidjungs <waewprach.s@ku.th>
2018-10-14 15:40:56 +0700

    map centroid position to centroid data array

--- kmeans/kmeans.c
+++ kmeans/kmeans.c
@@ -6,16 +6,43 @@
 int main(int argc, char *argv[]) {
   char filename[50];
   sprintf(filename, "./%s", argv[1]);
-  // FILE* stream = fopen(filename, "r");
 
   int row = atoi(argv[2]);
   int col = atoi(argv[3]);
   int k = atoi(argv[4]);
 
-  // random k centroids with unique position
-  int *centroids  = (int *)malloc(k * sizeof(int *));
+  // reading csv to array
+  int **data = (int **)malloc(row * sizeof(int *));
+  for (int i=0; i<row; i++)
+    data[i] = (int *)malloc(col * sizeof(int));
+
+  char line[1024];
+  int i = 0;
+  FILE* stream = fopen(filename, "r");
+  while(fgets(line, 1024, stream))
+  {
+    char *pch;
+    pch = strtok(line,",");
+    int j = 0;
+    while(pch != NULL)
+    {
+      data[i][j] = atoi(pch);
+      pch = strtok(NULL, ",");
+      j++;
+    }
+    i++;
+  }
+  for (int i = 0; i<row; i++) {
+    for (int j = 0; j<col; j++) {
+      printf("%d ", data[i][j]);
+    }
+    printf("\n");
+  }
+
+    // random k centroids with unique position
+  int *centroidPositions  = (int *)malloc(k * sizeof(int *));
   for (int i=0; i<k; i++) {
-    centroids[i] = -1;
+    centroidPositions[i] = -1;
   }
   srand(time(NULL));
   int ct = 0;
@@ -23,45 +50,38 @@ int main(int argc, char *argv[]) {
     int r = rand() % row;
     int dup = 0;
     for (int i=0; i<=ct; i++) {
-      if (r == centroids[i]) {
+      if (r == centroidPositions[i]) {
         dup = 1;
         break;
       }
     }
     if (dup == 0) {
-      centroids[ct] = r;
+      centroidPositions[ct] = r;
       ct++;
     }
   }
-  // for (int i=0; i<k; i++) {
-  //   printf("%d\n", centroids[i]);
-  // }
+  for (int i=0; i<k; i++) {
+    printf("%d\n", centroidPositions[i]);
+  }
 
-  // int **data = (int **)malloc(row * sizeof(int *));
-  // for (int i=0; i<row; i++)
-  //   data[i] = (int *)malloc(col * sizeof(int));
+  float **centroids = (float **)malloc(k * sizeof(float *));
+  for (int i=0; i<k; i++)
+    centroids[i] = (float *)malloc(col * sizeof(float));
 
-  // char line[1024];
-  // int i = 0;
-  // while(fgets(line, 1024, stream))
-  // {
-  //   char *pch;
-  //   pch = strtok(line,",");
-  //   int j = 0;
-  //   while(pch != NULL)
-  //   {
-  //     data[i][j] = atoi(pch);
-  //     pch = strtok(NULL, ",");
-  //     j++;
-  //   }
-  //   i++;
-  // }
-  // for (int i = 0; i<row; i++) {
-  //   for (int j = 0; j<col; j++) {
-  //     printf("%d ", data[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  // free(data);
+  for (int i=0; i<k; i++) {
+    for (int j=0; j<col; j++) {
+      centroids[i][j] = data[centroidPositions[i]][j];
+    }
+  }
+
+ for (int i=0; i<k; i++) {
+    for (int j=0; j<col; j++) {
+      printf("%f ", centroids[i][j]);
+    }
+    printf("\n");
+  }
+  free(data);
+  free(centroidPositions);
+  free(centroids);
   return 0;
 }
